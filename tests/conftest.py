07fd9998101bf97f9001b9c17534d9792f0e7e45
Ilya Bagrov <ilya.b.only@gmail.com>
2024-12-02 02:06:41 +0300

    Add tests for utils module

---
+++ tests/conftest.py
@@ -0,0 +1,52 @@
+import os
+import sys
+sys.path.insert(1, os.path.dirname(sys.path[0]))
+
+from typing import Generator
+
+import pytest
+
+from src.entities import Task
+
+
+PATH_TO_TEST_FILE = 'test_tasks.csv'
+
+
+@pytest.fixture
+def create_test_storage() -> Generator[str, None, None]:
+    """Create a test storage file and yield the path to this file. Delete the
+    created file after the test is completed.
+    """
+
+    test_data = [
+        '0123id,title,description,category,due_date,priority,status\n',
+        '1,Изучить основы FastAPI,Пройти документацию по FastAPI и создать простой проект,Обучение,2024-11-30,Высокий,Не выполнена\n',
+        '2,Изучить основы Django,Пройти документацию по Django и создать простой проект,Обучение,2024-07-20,Высокий,Не выполнена\n',
+    ]
+    with open(PATH_TO_TEST_FILE, 'w') as f:
+        f.writelines(test_data)
+    yield PATH_TO_TEST_FILE
+    os.remove(PATH_TO_TEST_FILE)
+
+@pytest.fixture
+def create_test_tasks() -> tuple[Task, Task]:
+    """Create two different Task objects."""
+    task1 = Task(
+        id=1,
+        title='Изучить основы FastAPI',
+        description='Пройти документацию по FastAPI и создать простой проект',
+        category='Обучение',
+        due_date='2024-11-30',
+        priority='Высокий',
+        status='Не выполнена'
+    )
+    task2 = Task(
+        id=2,
+        title='Изучить основы Django',
+        description='Пройти документацию по Django и создать простой проект',
+        category='Обучение',
+        due_date='2024-07-20',
+        priority='Высокий',
+        status='Не выполнена'
+    )
+    return task1, task2
---
+++ tests/test_utils.py
@@ -0,0 +1,46 @@
+from src.entities import StorageState
+from src import utils
+
+
+def test_get_storage_state(create_test_storage, create_test_tasks):
+    task1, task2 = create_test_tasks
+    expected_state = StorageState(id_count=123, tasks=[task1, task2])
+    state = utils.get_storage_state(create_test_storage)
+    assert state == expected_state
+
+
+def test_save_storage_state(create_test_storage, create_test_tasks):
+    task1, task2 = create_test_tasks
+    task1.status = 'Выполнена'
+    task2.priority = 'Обычный'
+    new_state = StorageState(id_count=23, tasks=[task1, task2])
+
+    path = create_test_storage
+    utils.save_storage_state(path, new_state)
+
+    expected_data = (
+        '0023id,title,description,category,due_date,priority,status\n'
+        '1,Изучить основы FastAPI,Пройти документацию по FastAPI и создать простой проект,Обучение,2024-11-30,Высокий,Выполнена\n'
+        '2,Изучить основы Django,Пройти документацию по Django и создать простой проект,Обучение,2024-07-20,Обычный,Не выполнена\n'
+    )
+    with open(path, 'r') as f:
+        saved_state = f.read()
+    assert saved_state == expected_data
+
+
+def test_get_id_count(create_test_storage):
+    expected_id_count = 123
+    id_count = utils.get_id_count(create_test_storage)
+    assert id_count == expected_id_count
+
+
+def test_save_id_count(create_test_storage):
+    path = create_test_storage
+    id_count = 12
+    utils.save_id_count(path, id_count)
+    with open(path, 'r') as f:
+        saved_id_count = int(f.read(utils.ID_MAX_BYTES_SIZE))
+        other_data = f.read()
+    assert saved_id_count == id_count, \
+        f'Expected value = {id_count}, saved value = {saved_id_count}'
+    assert other_data, 'All tasks were lost'
